9693c41312a8c283b2b77309be2f62ecf0ba1c81
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-04-13 13:07:02 +0900

    player 동기화 component

---
+++ Photon/Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class Player : MonoBehaviourPunCallbacks, IPunObservable
+{
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
