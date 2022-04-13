989857d718a3c57bf3e5eae3100da88214ff7ae8
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-04-13 15:44:44 +0900

    Bullet 동기화 Component

---
+++ Photon/Assets/Scripts/Bullet.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
+
+
+public class Bullet : MonoBehaviourPunCallbacks , IPunObservable
+{
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
