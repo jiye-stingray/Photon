d717a6af6140b3664703776f04ef84e84dcf21f1
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-04-15 22:43:42 +0900

    닉네임 표시 방법

--- Photon/Assets/Scripts/Player.cs
+++ Photon/Assets/Scripts/Player.cs
@@ -1,11 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using Photon.Pun;
 using Photon.Realtime;
 
 public class Player : MonoBehaviourPunCallbacks, IPunObservable
 {
+    public Rigidbody2D rigid;
+    public Animator anim;
+    public SpriteRenderer sr;
+    public PhotonView pv;
+    public Text NickNameText;
+    public Image HealthImage;
+
+    bool isGround;
+    Vector3 curPos;
+
+    void Awake()
+    {
+        //´Ð³×ÀÓ
+        NickNameText.text = pv.IsMine ? PhotonNetwork.NickName : pv.Owner.NickName;
+        NickNameText.color = pv.IsMine ? Color.red : Color.green;
+    }
+
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
         
