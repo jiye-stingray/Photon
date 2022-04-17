fe37c311ae057509b01a5e9b70b79452f34252f3
jiye-stingray <81548078+jiye-stingray@users.noreply.github.com>
2022-04-17 12:04:13 +0900

    x축 반전

--- Photon/Assets/Scripts/Player.cs
+++ Photon/Assets/Scripts/Player.cs
@@ -23,6 +23,25 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
         NickNameText.text = pv.IsMine ? PhotonNetwork.NickName : pv.Owner.NickName;
         NickNameText.color = pv.IsMine ? Color.green : Color.red;
     }
+    void Update()
+    {
+        if (pv.IsMine)
+        {
+            // <- -> 이동
+            float axis = Input.GetAxisRaw("Horizontal");
+            rigid.velocity = new Vector2(4 * axis, rigid.velocity.y);
+
+            if (axis != 0)
+            {
+                anim.SetBool("walk", true);
+                pv.RPC("FlipXRPC", RpcTarget.AllBuffered, axis);    //재접속시 filpx를 동기화 시켜주기 위해서 AllBuffed
+            }
+            else anim.SetBool("walk", false);
+        }
+    }
+
+    [PunRPC]
+    void FlipXRPC(float axis) => sr.flipX = axis == -1;
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
@@ -35,13 +54,5 @@ public class Player : MonoBehaviourPunCallbacks, IPunObservable
         
     }
 
-    void Update()
-    {
-        if (pv.IsMine)
-        {
-            // <- -> 이동
-            float axis = Input.GetAxisRaw("Horizontal");
-            rigid.velocity = new Vector2(4 * axis , rigid.velocity.y);
-        }
-    }
+
 }
